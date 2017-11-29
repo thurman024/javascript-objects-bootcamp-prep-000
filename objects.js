6629fe6112c51ce17815c3426d01ada9a185a247
Stephen Thurman <thurman024@gmail.com>
2017-11-29 02:56:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(object, artist, song) {
   Object.assign(object, [artist] : [song])
   return object
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
