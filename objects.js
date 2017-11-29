20f51199f42aee8830c2ed381bdaa77ece161af0
Stephen Thurman <thurman024@gmail.com>
2017-11-29 02:59:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'RHCP' : 'Snow'}
 
 function updatePlaylist(object, artist, song) {
-  Object.assign(object, [artist] : [song])
+  Object.assign(object, {[artist] : [song]})
   return object
 }
 
